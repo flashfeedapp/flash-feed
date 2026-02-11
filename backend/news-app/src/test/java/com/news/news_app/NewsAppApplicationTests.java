134939aff6c79adec30c8e6bab01481e32816056
Flash Feed <flashfeed.app@gmail.com>
2026-02-11 17:43:11 +0530

    Add new empty Spring Boot backend project

---
+++ backend/news-app/src/test/java/com/news/news_app/NewsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news.news_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
