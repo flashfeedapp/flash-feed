12beb84f2307552351b91ad24b7815a0cf60ce72
Flash Feed <flashfeed.app@gmail.com>
2026-02-12 07:03:19 +0530

    Update backend project yml file12

---
+++ backend/news-app/src/main/java/com/news/news_app/configuration/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package com.news.news_app.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://flash-feed-production-1da5.up.railway.app"));
+    }
+}
