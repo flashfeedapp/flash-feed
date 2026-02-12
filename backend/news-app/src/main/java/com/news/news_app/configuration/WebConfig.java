e2a38e424d0b6401e51a451ba1c288b6fb1c68dc
Flash Feed <flashfeed.app@gmail.com>
2026-02-12 06:52:30 +0530

    Update backend project yml file10

--- backend/news-app/src/main/java/com/news/news_app/configuration/WebConfig.java
+++ backend/news-app/src/main/java/com/news/news_app/configuration/WebConfig.java
@@ -15,8 +15,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
                         .allowedHeaders("*");
             }
         };
